ff145ad9c62459eb64d2eaf9db16de2d57afaf0f
Cody Rayburn <rayburn.cody12@gmail.com>
2020-10-07 16:42:40 -0400

    Adding Contact form

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,9 @@
-// Select DOM Items
+// Menu Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
+const menuContact = document.querySelector('.menu-contact');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('fields');
 
 //set initial state of menu
 let showMenu = false;
@@ -14,7 +14,7 @@ function toggleMenu() {
     if(!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
-        menuNav.classList.add('show');
+        menuContact.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
@@ -23,7 +23,7 @@ function toggleMenu() {
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
-        menuNav.classList.remove('show');
+        menuContact.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
@@ -31,3 +31,32 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+// Wrap every letter in a span
+/*var textWrapper = document.querySelector('.ml14 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml14 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeInOutExpo",
+    duration: 900
+  }).add({
+    targets: '.ml14 .letter',
+    opacity: [0,1],
+    translateX: [40,0],
+    translateZ: 0,
+    scaleX: [0.3, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=600',
+    delay: (el, i) => 150 + 25 * i
+  })/* .add({
+    targets: '.ml14',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });*/
